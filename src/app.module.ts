a56b12995db38d4a909061f3ba3ab3f3dcb70aac
Nhilesh <66257640+Nhilesh@users.noreply.github.com>
2020-07-15 13:49:58 +0530

    Changes for Graphql And TypeORM support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { FlightsModule } from './flights/flights.module';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      playground: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'SqlAdmin$123',
+      database: 'flight_db',
+      entities: [
+        "dist/**/*.entity.{ts,js}"
+      ],
+      synchronize: true,
+    }),
+    FlightsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
